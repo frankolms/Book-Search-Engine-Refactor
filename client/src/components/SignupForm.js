dc5cc980a71ea55d455eab34c04460ef20ba3094
frankolms <frankolms@gmail.com>
2023-06-25 14:07:08 -0400

    changed name of mutation variable in signupform.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 
+import Auth from "../utils/auth";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
-import Auth from "../utils/auth";
 
 const SignupForm = () => {
   // set initial form state
@@ -17,7 +17,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [createUser] = useMutation(ADD_USER);
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -35,7 +35,7 @@ const SignupForm = () => {
     }
 
     try {
-      const { data } = await createUser({
+      const { data } = await addUser({
         variables: { ...userFormData },
       });
 
